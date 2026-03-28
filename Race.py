c6877c3a93faeed8fb53158704db26f22c9005e6
SungjuMa <u1615846@utah.edu>
2026-03-28 19:51:40 +0900

    A2 file
    My code for Assignment 2

---
+++ Race.py
@@ -0,0 +1,79 @@
+from graphics import *
+from Dice import Dice
+import time
+
+class Horse:
+    def __init__(self,y,image,window):
+        self.x = 50
+        self.y = y
+        self.image = image
+        self.window = window
+        self.dice = Dice(6)
+
+    def move(self):
+        roll_value = self.dice.roll()
+        self.x += roll_value
+
+    def draw(self):
+        try:
+            self.image.undraw()
+        except:
+            pass
+        self.image.draw_at_pos(self.window,self.x, self.y)
+    def crossed_finish_line(self, finsih_x):
+        return self.x >= finsih_x
+
+def main():
+    win = GraphWin("Horse Race", 700,700)
+    try:
+        win.autoflush = False
+    except:
+        pass
+    horse1_img = Image(Point(0,0), "horse1_img.png")
+    horse2_img = Image(Point(0,0), "horse2_img.png")
+    horse3_img = Image(Point(0,0), "horse3_img.png")
+    horse1 = Horse(150, horse1_img, win)
+    horse2 = Horse(350, horse2_img, win)
+    horse3 = Horse(550, horse3_img, win)
+    finsh_x = 600
+    finsh_line = Line(Point(finsh_x, 0), Point(finsh_x,700))
+    horse1.draw()
+    horse2.draw()
+    horse3.draw()
+    finsh_line.draw(win)
+    win.getMouse()
+    race_over = False
+    while not race_over:
+        horse1.move()
+        horse2.move()
+        horse3.move()
+        horse1.draw()
+        horse2.draw()
+        horse3.draw()
+        try:
+            update()
+        except:
+            pass
+        h1 = horse1.crossed_finish_line(finsh_x)
+        h2 = horse2.crossed_finish_line(finsh_x)
+        h3 = horse3.crossed_finish_line(finsh_x)
+        if h1 or h2 or h3:
+            race_over = True
+        time.sleep(0.06)
+    if h1:
+        print("horse 1 win")
+    elif h2:
+        print("horse 2 win")
+    elif h3:
+        print("horse 3 win")
+    elif h1 and h2:
+        print("draw")
+    elif h1 and h3:
+        print("draw")
+    elif h3 and h2:
+        print("draw")
+
+    win.getMouse()
+    win.close()
+if __name__ == "__main__":
+    main()
