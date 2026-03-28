1dad84e44a8c37574f4f797486d1448f18185075
SungjuMa <u1615846@utah.edu>
2026-03-28 20:04:56 +0900

    Sungju Ma A2 code
    I changed lots of error..s plz i want to finish my assignment :[

--- Race.py
+++ Race.py
@@ -1,14 +1,16 @@
+import random
+
 from graphics import *
 from Dice import Dice
 import time
 
 class Horse:
-    def __init__(self,y,image,window):
+    def __init__(self,speed,y,image,window):
         self.x = 50
         self.y = y
         self.image = image
         self.window = window
-        self.dice = Dice(6)
+        self.dice = Dice(speed)
 
     def move(self):
         roll_value = self.dice.roll()
@@ -32,9 +34,9 @@ def main():
     horse1_img = Image(Point(0,0), "horse1_img.png")
     horse2_img = Image(Point(0,0), "horse2_img.png")
     horse3_img = Image(Point(0,0), "horse3_img.png")
-    horse1 = Horse(150, horse1_img, win)
-    horse2 = Horse(350, horse2_img, win)
-    horse3 = Horse(550, horse3_img, win)
+    horse1 = Horse(random.randint(6,10),150, horse1_img, win)
+    horse2 = Horse(random.randint(6,10),350, horse2_img, win)
+    horse3 = Horse(random.randint(6,10), 550, horse3_img, win)
     finsh_x = 600
     finsh_line = Line(Point(finsh_x, 0), Point(finsh_x,700))
     horse1.draw()
@@ -44,6 +46,8 @@ def main():
     win.getMouse()
     race_over = False
     while not race_over:
+        if win.isClosed():
+            break
         horse1.move()
         horse2.move()
         horse3.move()
@@ -60,20 +64,19 @@ def main():
         if h1 or h2 or h3:
             race_over = True
         time.sleep(0.06)
-    if h1:
+    if (h1 and h2) or (h1 and h3) or (h2 and h3):
+        print("draw")
+    elif h1:
         print("horse 1 win")
     elif h2:
         print("horse 2 win")
     elif h3:
         print("horse 3 win")
-    elif h1 and h2:
-        print("draw")
-    elif h1 and h3:
-        print("draw")
-    elif h3 and h2:
-        print("draw")
 
-    win.getMouse()
+    try:
+        win.getMouse()
+    except:
+        pass
     win.close()
 if __name__ == "__main__":
     main()
